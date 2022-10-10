ec8e476035fe9cf16f7f61dbe23e444d2468d140
chloed7 <114586399+chloed7@users.noreply.github.com>
2022-10-10 11:18:32 -0400

    Update TipCalculator.java

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, double tipPercentage) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+    }
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+
+    public double getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount() {
+        return Math.round((totalBillBeforeTip * (tipPercentage / 100))*100.0)/100.0;
+    }
+
+    public double totalBill() {
+        return totalBillBeforeTip + tipAmount();
+    }
+
+    public double perPersonCostBeforeTip() {
+        return Math.round((totalBillBeforeTip / numPeople)*100.0)/100.0;
+    }
+
+    public double perPersonTipAmount() {
+        return Math.round((tipAmount() / numPeople)*100.0)/100.0;
+    }
+
+    public double perPersonTotalCost() {
+        return Math.round((totalBill() / numPeople)*100.0)/100.0;
+    }
 }
